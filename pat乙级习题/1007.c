36e855a97a2b5df686501a248da7c01d6941ed78
lazylazyboy <494454524@qq.com>
2017-11-16 01:10:47 +0800

    11/26

--- pat乙级习题/1007.c
+++ pat乙级习题/1007.c
@@ -6,13 +6,12 @@ int main()
 	int n;
 	scanf("%d", &n); 
 	int cnt = 0;
-	for (int i = 2;i <= n;i++)
+	for (int i = 2;i < n-1;i++)
 	{
 		if (isPrime(i) && isPrime(i + 2))
 			cnt++;
 	}
 	printf("%d", cnt);
-	getch();
 	return 0;
 }
 int isPrime(int x)
