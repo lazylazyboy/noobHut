c325d51da752d36a8b6576eeedf49d1e0dde6034
lazylazyboy <494454524@qq.com>
2017-11-16 12:50:52 +0800

    11/26

--- pat乙级习题/1007.c
+++ pat乙级习题/1007.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
+#include <math.h>
 int isPrime(int x);
 int main()
 {
 	int n;
 	scanf("%d", &n); 
 	int cnt = 0;
-	for (int i = 2;i < n-1;i++)
+	for (int i = 3;i < n-1;i+=2)
 	{
 		if (isPrime(i) && isPrime(i + 2))
 			cnt++;
@@ -17,7 +17,7 @@ int main()
 int isPrime(int x)
 {
 	int ret = 1;
-	for (int i = 2;i < x;i++)
+	for (int i = 2;i <= sqrt(x);i++)	//此处循环到x的平方根处就足够判断是否是素数了,减少时间消耗
 	{
 		if (x%i == 0)
 		{
